dc0019f2b9895600bee1b056b035fa4ef7f27493
Emperor-Penguin-3 <156144412+Emperor-Penguin-3@users.noreply.github.com>
2025-10-29 22:34:44 -0400

    Add files via upload

---
+++ Task_3_1.m
@@ -0,0 +1,23 @@
+V1_raw = load("C:\Users\fwbla\Downloads\Project2DataFiles\Project2DataFiles\Parameters_V1_1.mat");
+V2_raw = load("C:\Users\fwbla\Downloads\Project2DataFiles\Project2DataFiles\Parameters_V2_1.mat");
+MoCap = load("C:\Users\fwbla\Downloads\Project2DataFiles\Project2DataFiles\mocapPoints3D.mat"); % load all relevant information related to computation
+
+V1 = V1_raw.Parameters;
+V2 = V2_raw.Parameters;
+
+R1 = double(V1.Rmat); % load relevant computational parameters inside function
+P1 = double(V1.Pmat); % Pmat is not the real Pmat, rather the E or F matrix, E in this context because we know the intrinsic parameters
+Point1 = double(V1.position);
+
+R2 = double(V2.Rmat); % most importantly, convert information from structural to double for proper manipulation of data
+P2 = double(V2.Pmat);
+Point2 = double(V2.position);
+
+t1 = - R1 * Point1';
+t2 = - R2 * Point2';
+
+P_exp_1 = [R1, t1];
+P_exp_2 = [R2, t2];
+
+P_result_1 = P_exp_1 - P1;
+P_result_2 = P_exp_2 - P2;
